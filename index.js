45de138c99462bc36c1ddf86f87dc84b389b7cd9
denisdelaki <denisdelaki@gmail.com>
2022-06-09 10:43:28 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+let cards = [];
+function writeCards(arraycards) {
+  for (let i = 0; i < arraycards.length; i++) {
+    cards.push(`Thank you, ${arraycards[i]}, for the wonderful surprise gift!`);
+  }
+  return cards;
+}
+function countDown() {
+  let countDown = 10;
+  while (countDown >= 0) {
+    console.log(countDown--);
+  }
+}
